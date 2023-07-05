d8b7e9db62bf1c7f43cde77894bfad92ec4a34ef
Kiddooo <29415728+Kiddooo@users.noreply.github.com>
2023-07-06 00:11:31 +0100

    Potential 1.20 update

--- example.py
+++ example.py
@@ -1,27 +1,22 @@
 import minecraft_data
 # Java edition minecraft-data
-mcd = minecraft_data("1.13")
+mcd = minecraft_data("1.20.1")
 
-print(mcd.version)
+print(str(mcd.version) + "\n")
 
 print(mcd.find_item_or_block(1))
-print(mcd.find_item_or_block('stone'))
+print(str(mcd.find_item_or_block('stone')) + "\n")
 
-print(mcd.recipes['5'][0])
+print(str(mcd.recipes['5'][0]) + "\n")
 
-print(mcd.windows['minecraft:brewing_stand'])
+print(str(mcd.windows['minecraft:brewing_stand']) + "\n")
 
-print(mcd.effects_name['Haste'])
+print(str(mcd.effects_name['Haste']) + "\n")
 
-# Bedrock Edition minecraft-data
-mcd_pe = minecraft_data("1.0", "bedrock")
 
-print(mcd_pe.version)
-print(mcd_pe.find_item_or_block('stone'))
-
-# Query common data. E.g. to map the protocol version to a minecraft version
-protocol_version = 754 # example protocol version
-for version in minecraft_data.common().protocolVersions:
-    if version["version"] == protocol_version:
-        print(version["minecraftVersion"]) # 1.16.5
-        break
+# # Query common data. E.g. to map the protocol version to a minecraft version
+# protocol_version = 754 # example protocol version
+# for version in minecraft_data.common().protocolVersions:
+#     if version["version"] == protocol_version:
+#         print(version["minecraftVersion"]) # 1.16.5
+#         break
